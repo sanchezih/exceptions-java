746bd6f525f6cff0ee9a2ffd62873b12dc6507e1
Ignacio Sanchez <sanchezih@gmail.com>
2024-07-20 12:08:28 -0300

    update

--- src/main/java/com/github/sanchezih/excepciones/_02_throw/Test6.java
+++ src/main/java/com/github/sanchezih/excepciones/_02_throw/Test6.java
@@ -12,10 +12,13 @@ public class Test6 {
 
 	public static void main(String[] args) {
 		int y, z;
+
 		System.out.print("Ingrese y: ");
 		y = new Scanner(System.in).nextInt();
+
 		z = new Calculadora().factorial(y);
 		System.out.println("Resultado: " + z);
+
 		System.out.println("Adios");
 
 	}
